8d93bedbb81e035948838aed2c08b0c056a13897
Tim <varloc2000@gmail.com>
2015-09-23 17:05:42 +0300

    Config app parameters.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,9 +16,9 @@ class DefaultController extends Controller
 
        $manager = $this->get('clarity_yandex.passport.manager');
 
-       $response = $manager->getAuthorizationCode('my_first_app', 'main');
+       $response = $manager->getAuthorizationCode('my_first_app', 'my_first_app');
 
-       die(var_dump($response));
+       // die(var_dump($response));
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
