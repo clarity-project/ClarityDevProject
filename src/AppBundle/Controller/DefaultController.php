d94a63eef131afff5544fdb977efd7a896a1bed0
Vladislav <13thmerlin@gmail.com>
2015-09-25 03:29:51 +0300

    add code response

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,8 +17,8 @@ class DefaultController extends Controller
        $manager = $this->get('clarity_yandex.oauth.manager');
 
        $response = $manager->getAuthorizationCode('my_first_app', 'my_first_app');
-
-       // die(var_dump($response));
+//
+        die(var_dump($response));
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
