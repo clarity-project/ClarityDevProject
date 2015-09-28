528c42a786760f8165ac14100feae9b46455a836
Tim <varloc2000@gmail.com>
2015-09-28 23:03:21 +0300

    1. Add action to check cached token. 2. Add finish action and add redirect_route parameter to configuration.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,27 @@ class DefaultController extends Controller
     {
         return $this->redirectToRoute('clarity_yandex_oauth_token_request', array(
             'appName' => 'my_first_app',
-            'scope' => 'main'
+            'scope' => 'main',
+//            'device_id' => 'ios_id',
+//            'device_name' => 'my_ios'
         ));
+    }
+
+    /**
+     * @Route("/cached", name="cached")
+     */
+    public function cachedTokenAction()
+    {
+        $appToken = $this->get('clarity_yandex.oauth.service')->getCachedToken('my_first_app', 'main');
+
+        die(var_dump($appToken));
+    }
+
+    /**
+     * @Route("/finish", name="finish")
+     */
+    public function finishAction()
+    {
+        return $this->render('AppBundle:Default:finish.html.twig');
     }
 }
