50b405652bf0d94d750752720e50f713221cf0d4
Tim <varloc2000@gmail.com>
2015-09-30 18:39:48 +0300

    Change configuration for oauth vendor. Update vendor.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,16 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         return $this->redirectToRoute('clarity_yandex_oauth_token_request', array(
             'appName' => 'my_first_app',
-            'scope' => 'main',
 //            'device_id' => 'ios_id',
 //            'device_name' => 'my_ios'
         ));
@@ -26,7 +24,7 @@ class DefaultController extends Controller
      */
     public function cachedTokenAction()
     {
-        $appToken = $this->get('clarity_yandex.oauth.service')->getCachedToken('my_first_app', 'main');
+        $appToken = $this->get('clarity_yandex.oauth.service')->getCachedToken('my_first_app');
 
         die(var_dump($appToken));
     }
